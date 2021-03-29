d389560823d85bb8d9910299b06cd6e319d7bb7e
Nishat009 <nishatraihana009@gmail.com>
2021-03-29 13:39:10 +0600

    root api created

--- index.js
+++ index.js
@@ -16,6 +16,10 @@ app.use(cors());
 const port = 5000
 
 
+app.get('/',(req,res)=>{
+    res.send('hello from db')
+})
+
 
 //from mongo db cluster-connect 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.ktoki.mongodb.net/${process.env.DB_NAME}?retryWrites=true&w=majority`;
